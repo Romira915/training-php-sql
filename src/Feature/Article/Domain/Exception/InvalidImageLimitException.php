b9ada35f2149a7bb7a95fc33de356ab10e4ea1c3
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-05-23 11:33:58 +0900

    fix: Exceptionの引数を親と同じようにする

--- src/Feature/Article/Domain/Exception/InvalidImageLimitException.php
+++ src/Feature/Article/Domain/Exception/InvalidImageLimitException.php
@@ -6,11 +6,12 @@ namespace Romira\Zenita\Feature\Article\Domain\Exception;
 
 
 use Exception;
+use Throwable;
 
 class InvalidImageLimitException extends Exception
 {
-    public function __construct(string $message = '画像のアップロード数が不正です。')
+    public function __construct(string $message, int $code = 0, Throwable $previous = null)
     {
-        parent::__construct($message);
+        parent::__construct($message, $code, $previous);
     }
 }
