bbce9f2cbb805a9a1b9d8b94e32437a0be187285
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-05-24 09:13:08 +0900

    fix: add strict_types=1

--- src/Feature/Article/Domain/Repositories/ImageStorageInterface.php
+++ src/Feature/Article/Domain/Repositories/ImageStorageInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Romira\Zenita\Feature\Article\Domain\Repositories;
 
 use Romira\Zenita\Feature\Article\Interfaces\Exception\InvalidUploadImageException;
